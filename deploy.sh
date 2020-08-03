9ea697fa4bfa3e850ad207bcd7b65357f3b6f0c2
Arya Karnik <aryakarnik4@gmail.com>
2020-08-03 17:21:37 -0500

    added deploy steps

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# pulls recent code
 git pull
+
+# if running, kills current bot version
+docker rm --force helixbot
+
+# builds new bot with applied code changes
+docker build -t "bot" .
+
+# runs new bot with applied code changes
+docker run --detach --name helixbot bot
